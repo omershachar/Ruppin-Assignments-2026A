d11a4a6662a21202cf00d2d4f2b45729c99deacf
Omer <omershachar98@icloud.com>
2025-11-13 14:01:27 +0200

    added a networking pr project in eclipse for testing

---
+++ JavaStaff/EclipseProjects/simpleNetworking/src/simpleNetworking/Main.java
@@ -0,0 +1,7 @@
+package simpleNetworking;
+
+public class Main {
+	public static void main(String []args) {
+		System.out.println("HI");
+	}
+}
---
+++ JavaStaff/EclipseProjects/simpleNetworking/src/simpleNetworking/SimpleClient.java
@@ -0,0 +1,14 @@
+package simpleNetworking;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class SimpleClient {
+ public static void main(String[] args) {
+     try (Socket socket = new Socket("localhost", 5500)) {
+         System.out.println("Connected to the server");
+     } catch (IOException e) {
+         e.printStackTrace();
+     }
+ }
+} //end of class
