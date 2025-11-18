231317b74fae5b7fb74902a066332bb3cecf4545
omer <omershachar98@icloud.com>
2025-11-18 10:08:10 +0200

    tidy repo

--- JavaStaff/EclipseProjects/simpleNetworking/src/simpleNetworking/SimpleClient.java
+++ JavaStaff/EclipseProjects/simpleNetworking/src/simpleNetworking/SimpleClient.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 
 public class SimpleClient {
  public static void main(String[] args) {
-     try (Socket socket = new Socket("localhost", 5500)) {
+     try (Socket socket = new Socket("localhost", 5000)) {
          System.out.println("Connected to the server");
      } catch (IOException e) {
          e.printStackTrace();
--- JavaStaff/EclipseProjects/simpleNetworking/src/simpleNetworking/SimpleServer.java
+++ JavaStaff/EclipseProjects/simpleNetworking/src/simpleNetworking/SimpleServer.java
@@ -7,12 +7,16 @@ import java.net.Socket;
 public class SimpleServer {
 
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(5500)) {
-            System.out.println("Server is listening on port 5500");
-            Socket socket = serverSocket.accept();
-            System.out.println("new client connected");
-        } catch (IOException e) {
-            e.printStackTrace();
+        while(true) {
+            try(ServerSocket serverSocket = new ServerSocket(5000)) {
+                System.out.println("Server is listening on port 5000");
+                Socket socket = serverSocket.accept();
+                System.out.println("new client connected");
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println("Stopped listening");
     }
 } //end of class
