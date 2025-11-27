f74f97e941e2be7842c8230e65e17761f8d581a8
aviv-itzhak <aviv96xx@gmail.com>
2025-11-27 12:25:18 +0200

    Test

--- JavaStaff/1A-Java/Assignment-1-Java/src/Main.java
+++ JavaStaff/1A-Java/Assignment-1-Java/src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Hi
 void main() {
 
     ArrayList<Message> arrayList = new ArrayList<Message>();
