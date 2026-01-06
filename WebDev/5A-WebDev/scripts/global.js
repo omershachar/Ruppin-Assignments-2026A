00fc4b31ef2253ba9600f91fd192bf3c62a06ecb
omer <omershachar98@icloud.com>
2026-01-06 16:54:15 +0200

    edited basic website appearance and structure

---
+++ WebDev/5A-WebDev/scripts/global.js
@@ -0,0 +1,5 @@
+// Navigation methods
+const navigateToDashboard = () => {window.location.href = 'dashboard.html';}
+const navigateToRecharge = () => {window.location.href = 'recharge.html';}
+const navigateToOrders = () => {window.location.href = 'orders.html';}
+const navigateToCategories = () => {window.location.href = 'categories.html';}
