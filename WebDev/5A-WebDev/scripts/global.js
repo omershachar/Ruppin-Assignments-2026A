2c4efb58d1c77830944277fe16ff25248848601c
ItzhakAv <aviv96xx@gmail.com>
2026-01-15 23:41:10 +0200

    update A5 HTML

--- WebDev/5A-WebDev/scripts/global.js
+++ WebDev/5A-WebDev/scripts/global.js
@@ -3,3 +3,4 @@ const navigateToDashboard = () => {window.location.href = 'dashboard.html';}
 const navigateToRecharge = () => {window.location.href = 'recharge.html';}
 const navigateToOrders = () => {window.location.href = 'orders.html';}
 const navigateToCategories = () => {window.location.href = 'categories.html';}
+const navigateToManagement = () => {window.location.href = 'management.html';}
